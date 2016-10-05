d9b57ecc8a93b69b804d850f14573879065c682a
IttoKimura <kimura.itto.kd3@is.naist.jp>
2016-10-06 01:30:31 +0900

    クロール時の安定性を向上

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -8,7 +8,6 @@ from website_crawler import WebSiteCrawler
 DRIVER_PATH = '/home/itto-ki/Works/IEDriverServer.exe'
 
 
-
 def crawl_website(url):
     wsc = WebSiteCrawler(DRIVER_PATH, url)
     time.sleep(3)
@@ -16,12 +15,17 @@ def crawl_website(url):
 
 words_file = open('search_keywords_en.lst', 'r')
 for word in words_file.readlines()
-    gc = GoogleCrawler(DRIVER_PATH)
+    try:
+        gc = GoogleCrawler(DRIVER_PATH)
+    except:
+        continue
     try:
         gc.search(word)
+        print('Searching now - {}'.format(word))
         website_urls = gc.cite_website_urls()
         p = Pool(6)
         p.map(crawl_website, website_urls)
     except:
         sys.stderr.write('Cannot search for {}'.format(word))
+    p.terminate()
     gc.quit()
