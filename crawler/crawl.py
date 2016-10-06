8adb7c83825d57dead09f316abbf55ccfe80b74f
IttoKimura <kimura.itto.kd3@is.naist.jp>
2016-10-06 23:58:29 +0900

    クロール時の安定性を向上

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -11,7 +11,11 @@ DRIVER_PATH = '/home/itto-ki/Works/IEDriverServer.exe'
 def crawl_website(url):
     wsc = WebSiteCrawler(DRIVER_PATH, url)
     time.sleep(3)
-    wsc.quit()
+    try:
+        wsc.quit()
+    except:
+        sys.stderr.write('Cannot crawling {}'.format(url))
+
 
 words_file = open('search_keywords_en.lst', 'r')
 for word in words_file.readlines()
@@ -21,11 +25,20 @@ for word in words_file.readlines()
         continue
     try:
         gc.search(word)
+    except:
+        sys.stderr('Cannot open {} in GoogleCrawler'.format(word))
+        gc.close()
+        continue
+    try:
         print('Searching now - {}'.format(word))
         website_urls = gc.cite_website_urls()
-        p = Pool(6)
-        p.map(crawl_website, website_urls)
+        try:
+            p = Pool(6)
+            p.map(crawl_website, website_urls)
+            p.terminate()
+            p.join()
+        except:
+            sys.stderr.write('Unkown Error: Process stopeed. killing process')
     except:
         sys.stderr.write('Cannot search for {}'.format(word))
-    p.terminate()
     gc.quit()
